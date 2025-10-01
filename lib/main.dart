7445ca2566c94543cd6fb63341fe29d96b8b60ad
Muhammad Usama Ghafoor <m.usamaghafoor65@gmail.com>
2025-10-01 16:52:06 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,27 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(brightness: Brightness.dark,primarySwatch: Colors.blueGrey),
-      title: 'Covid-19',
-      home: const SplashScreen(),
-    );
+    return Builder(builder: (context) {
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+            brightness: Brightness.dark, primarySwatch: Colors.blueGrey),
+        title: 'Covid-19',
+        home: const SplashScreen(),
+        builder: (context, child) {
+          return GestureDetector(
+            onTap: () {
+              FocusScopeNode currentFocus = FocusScope.of(context);
+              if (!currentFocus.hasPrimaryFocus &&
+                  currentFocus.focusedChild != null) {
+                currentFocus.focusedChild!.unfocus();
+              }
+            },
+            behavior: HitTestBehavior.translucent,
+            child: child!,
+          );
+        },
+      );
+    });
   }
 }
